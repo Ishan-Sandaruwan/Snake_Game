9ed921f53b70423dc1fb7d86fd15b29eef81883e
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-11-12 22:51:39 +0530

    starting position added

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.goto(position)
+
+
 screen.exitonclick()
